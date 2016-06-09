bc458c64a4e0720dec66cc30dcab427d4ab6b5f3
Steve Brownlee <chortlehoort@gmail.com>
2016-06-09 10:31:52 -0500

    Can now add inventory attached to the currently authenticated user

--- app/controllers/NewFigurineController.js
+++ app/controllers/NewFigurineController.js
@@ -10,7 +10,12 @@ NotDolls.controller('NewFigurineController', [
 		$scope.figurine = {};
 
 		$scope.createFigurine = function () {
+
+			console.log(authFactory.getUser());
+
+			$scope.figurine.geekId = authFactory.getUser().GeekId;
 			$scope.figurine.createdDate = new Date();
+
 			$http({
 				url:'http://localhost:5000/api/Inventory',
 				method: 'POST',
